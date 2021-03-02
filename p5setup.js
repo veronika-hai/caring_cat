5fbaf4159b4883ba162d2d147620da12d8aab89a
veronika-hai <veronika.haidarow@stud.h-da.de>
2021-03-02 15:59:25 +0100

    scheiß auf canvas lol

--- p5setup.js
+++
@@ -1,9 +0,0 @@
-let sketch = new p5();
-let width = windowWidth;
-let height = windowHeight;
-
-function setup() {
-  sketch.createCanvas(windowWidth, windowHeight);
-  sketch.frameRate(30);
-}
-window.setup = setup;
